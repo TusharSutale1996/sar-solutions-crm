11ed9fd1c0b5a1f1fad4a8a799f3426f3d4926b8
Tushar Sutale <tushar.sutale@gmail.com>
2024-02-01 15:11:48 +0530

    project created

---
+++ sar-solutions-crm/src/main/java/com/sarsolutionscrm/SarSolutionsCrmApplication.java
@@ -0,0 +1,13 @@
+package com.sarsolutionscrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarSolutionsCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarSolutionsCrmApplication.class, args);
+	}
+
+}
